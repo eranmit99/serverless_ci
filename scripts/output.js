022a8cf9781441b2ee49db609b3682a2d2a2d4c9
eranmit <eran.mitrani@versatile.ai>
2021-06-08 18:18:18 +0300

    initial commit

--- scripts/output.js
+++ scripts/output.js
@@ -1,5 +1,19 @@
-function handler (data, serverless, options) {
-  console.log('Received Stack Output', data);
+const fs = require('fs');
+
+
+
+function getServerlessEndPointHash(data) {
+   return data.ServiceEndpoint.split('http://')[1].split('.')[0]
 }
+
+function handler (data, serverless, options) {
+   console.log("writing data to file");
+
+   fs.writeFileSync('./deploy-output.json', JSON.stringify({
+      apiHash: getServerlessEndPointHash(data)
+   }));
+   //console.log('Received Stack Output', data);
+}
+
 
 module.exports = { handler };
